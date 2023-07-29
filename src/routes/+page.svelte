22631d67d0c15150a52f6a725bf545df5ee77a46
Apricitine <apricitinecas@gmail.com>
2023-07-29 16:33:29 -0700

    yes

---
+++ src/routes/+page.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import { Rive } from "@rive-app/canvas";
+  import { onMount } from "svelte";
+
+  onMount(() => {
+    let r = new Rive({
+      src: "/intro.riv",
+      autoplay: true,
+      artboard: "Blobs",
+      stateMachines: "Animation",
+      canvas: document.getElementById("introCanvas") as HTMLCanvasElement,
+      onLoad: () => {
+        r.resizeDrawingSurfaceToCanvas();
+      },
+    });
+  });
+</script>
+
+<canvas id="introCanvas" width="300" height="300" />
+asdfdas
